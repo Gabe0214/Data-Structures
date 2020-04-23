224deb91ea5576256871a29e3e59b1ee0ef65bc6
Gabriel Anguiano <anguianog97@gmail.com>
2020-04-23 15:46:08 -0700

    mvp done

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -21,25 +21,25 @@ class BinarySearchTree:
         else:
             if self.right:
                 self.right.insert(value)
-                # if right does not exist make it the first right, lol.
+                # if right does not exist make it the first right.
             else:
                 self.right = BinarySearchTree(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        if target == self.value:
+        if self.value == target:
             return True
         if target < self.value:
-            if self.left is None:
+            if not self.left:
                 return False
             else:
-                self.contains(target)
+                return self.left.contains(target)
         if target > self.value:
-            if self.right is None:
+            if not self.right:
                 return False
             else:
-                self.contains(target)
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
@@ -47,7 +47,7 @@ class BinarySearchTree:
             return self.value
         else:
             # recursion on right pointer because values are greater
-            self.right.get_max()
+            return self.right.get_max()
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
@@ -56,32 +56,74 @@ class BinarySearchTree:
             cb(self.value)
              #if there's a left node, utilize recursion to callback the values
             if self.left:
-                self.for_each(cb)
+                self.left.for_each(cb)
             #same approach for the right side
             if self.right:
-                self.for_each(cb)
+                self.right.for_each(cb)
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if node == None:
+            return
+
+        self.in_order_print(node.left)
+        print(node.value)
+        self.in_order_print(node.right)
+
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        ## utilize queue
+        #initialize the queue
+        q = Queue()
+
+        #dequeue the node
+        q.enqueue(node)
+
+        while q.len() > 0:
+            # current node is being removed
+            current_node = q.dequeue()
+            #print the value
+            print(current_node.value)
+
+            if current_node.left:
+                # if the node has a left reference
+                q.enqueue(current_node.left)
+            if current_node.right:
+                #same thing as left
+                q.enqueue(current_node.right)
+
+
+
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        # use stack when implementing depth
+        stack = Stack()
+        stack.push(node)
+        # while the stack has a node
+        while stack.len() > 0:
+            current_node = stack.pop()
+            print(current_node.value)
+            # check that left nodes exist. if true, then push the value(s)
+            if current_node.left:
+                stack.push(current_node.left)
+            # same approach to the right side
+            if current_node.right:
+                stack.push(current_node.right)
+
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
 
-    # Print Pre-order recursive DFT
+    #Print Pre-order recursive DFT
     def pre_order_dft(self, node):
         pass
 
